8b7caa847478011f6048d661de213732d5c54acd
luizalinda <84139409+luizalinda@users.noreply.github.com>
2022-08-10 13:01:25 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+const conexao = async()=>{
+    const atlas = await mongoose.connect('mongodb+srv://userAT:AT2TRI@fiaptecnico.yk90q.mongodb.net/test')
+}
+
+const modelo = mongoose.Schema({
+    produto:String,
+    validade:Date
+})
+
+const produto = mongoose.model('produto',modelo)
+
+module.exports={conexao,produto}
---
+++ routes/cadastro.js
@@ -0,0 +1,19 @@
+const { produto, conexao } = require("../config/database")
+const { app } = require("../config/servidor")
+
+module.exports = (app)=>{
+    app.get("/cadastro", (req,res)=>{
+        res.render("cadastro.ejs")
+    })
+  
+    app.post('/cadastro', async (req,res)=>{
+        conexao()
+        const novo = await new produto({
+            produto: req.body.produto,
+            validade: req.body.validade
+        }).save()
+        res.redirect('/cadastro')
+    })
+  
+}
+
